e6c43b0880aa45f1ec9a4a6fc75dc4700a8efcd3
facSav <facu.savio@gmail.com>
2022-11-14 23:37:18 -0300

    trabajo corregido

--- index.js
+++ index.js
@@ -1,34 +1,7 @@
-class Contenedor {
-    constructor () {
-        this.productos = []
-    }
+const Contenedor = require('./modulo')
 
-    save (nombre, precio) {
-        this.productos.push({nombre: nombre, precio: precio, id: this.productos.length+1})
-    }
+const programa = new Contenedor ()
 
-    getAll (){
-        console.log(this.productos)
-    }
-
-    getById (id){
-        console.log(this.productos.find(item => item.id === id ))
-    }
-
-    deleteById (id){
-        const idParaEliminar = this.productos.findIndex(item => item.id === id)
-        this.productos.splice (idParaEliminar, 1)
-        return this.productos
-    }
-
-    deleteAll (){
-        this.productos = []
-    }
-}
-
-const programa = new Contenedor()
-
-const prueba = require('./modulo.js')
+programa.getAll()
 
 
-console.log(prueba.prueba())
--- modulo.js
+++ modulo.js
@@ -1,6 +1,30 @@
-function prueba (){
-    return 'hola'
+class Contenedor {
+    constructor () {
+        this.productos = []
+    }
+
+    save (nombre, precio) {
+        this.productos.push({nombre: nombre, precio: precio, id: this.productos.length+1})
+    }
+
+    getAll (){
+        console.log(this.productos)
+    }
+
+    getById (id){
+        console.log(this.productos.find(item => item.id === id ))
+    }
+
+    deleteById (id){
+        const idParaEliminar = this.productos.findIndex(item => item.id === id)
+        this.productos.splice (idParaEliminar, 1)
+        return this.productos
+    }
+
+    deleteAll (){
+        this.productos = []
+    }
 }
 
-module.exports = prueba
+module.exports = Contenedor
 
